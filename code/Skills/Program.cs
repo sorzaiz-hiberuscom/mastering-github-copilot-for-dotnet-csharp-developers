4801e7bde3304b164f5b3914f08b3ea5ca3a013e
sorzaiz-hiberuscom <sorzaiz@hiberus.com>
2026-04-15 10:38:17 +0000

    Agregar archivos iniciales de Program.cs y Skills.csproj

---
+++ code/Skills/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void CalculateNumbers(int var1, int var2)
+{       
+    int sum = var1 + var2;
+    int difference = var1 - var2;
+    int product = var1 * var2;
+    double quotient = (double)var1 / var2;
+
+    Console.WriteLine($"Sum: {sum}");
+    Console.WriteLine($"Difference: {difference}");
+    Console.WriteLine($"Product: {product}");
+    Console.WriteLine($"Quotient: {quotient}");
+}
